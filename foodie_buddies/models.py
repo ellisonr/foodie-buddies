e3b01041f4f31c9fdb080bf86c808e4f912a3191
Ellison Rio <rio.ellison@gmail.com>
2019-07-15 15:26:47 -0400

    changed price in menuitem model to DecimalField

--- foodie_buddies/models.py
+++ foodie_buddies/models.py
@@ -16,7 +16,7 @@ class Restaurant(models.Model):
 
 class MenuItem(models.Model):
     name = models.CharField(max_length=50)
-    price = models.CharField(max_length=10)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.CharField(max_length=250)
     image_url = models.TextField()
     restaurant = models.ForeignKey(
