5cc4b873adcafeb60e84b7b432ed59a6aa5b54bc
Ellison Rio <rio.ellison@gmail.com>
2019-07-15 14:53:31 -0400

    created django project and app, first migration of models

---
+++ foodie_buddies/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=50)
+    cuisine = models.CharField(max_length=50)
+    street_address = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.CharField(max_length=10)
+    description = models.CharField(max_length=250)
+    image_url = models.TextField()
+    restaurant = models.ForeignKey(
+        Restaurant, on_delete=models.CASCADE, related_name='menu_item')
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=50)
+    body = models.CharField(max_length=500)
+    menu_item = models.ForeignKey(
+        MenuItem, on_delete=models.CASCADE, related_name='comment')
+
+    def __str__(self):
+        return self.name
