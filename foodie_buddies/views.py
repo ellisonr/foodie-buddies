b709b3df020aaf388001b922d706cd21882aea9d
Ellison Rio <rio.ellison@gmail.com>
2019-07-15 23:37:45 -0400

    addmenuitem, added appropriate urls and views for add menu item

--- foodie_buddies/views.py
+++ foodie_buddies/views.py
@@ -3,6 +3,7 @@ from .forms import RestaurantForm, MenuItemForm, CommentForm
 from .models import Restaurant, MenuItem, Comment
 # Create your views here.
 
+
 def restaurant_list(request):
     restaurants = Restaurant.objects.all()
     return render(request, 'foodie_buddies/restaurant_list.html', {'restaurants': restaurants})
@@ -22,6 +23,7 @@ def menu_item_detail(request, pk):
     menu_item = MenuItem.objects.get(id=pk)
     return render(request, 'foodie_buddies/menu_item_detail.html', {'menu_item': menu_item})
 
+
 def restaurant_create(request):
     if request.method == 'POST':
         form = RestaurantForm(request.POST)
@@ -31,3 +33,14 @@ def restaurant_create(request):
     else:
         form = RestaurantForm()
     return render(request, 'foodie_buddies/restaurant_form.html', {'form': form})
+
+
+def menu_item_create(request):
+    if request.method == 'POST':
+        form = MenuItemForm(request.POST)
+        if form.is_valid():
+            menu_item = form.save()
+            return redirect('menu_item_detail', pk=menu_item.pk)
+    else:
+        form = MenuItemForm()
+    return render(request, 'foodie_buddies/menu_item_form.html', {'form': form})
