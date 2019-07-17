087c7410c3536b81949f0c171e26ebfc4ccadb8c
Ellison Rio <rio.ellison@gmail.com>
2019-07-17 09:40:47 -0400

    added views and urls for update and delete for all models

--- foodie_buddies/views.py
+++ foodie_buddies/views.py
@@ -14,19 +14,6 @@ def restaurant_detail(request, pk):
     return render(request, 'foodie_buddies/restaurant_detail.html', {'restaurant': restaurant})
 
 
-def menu_item_list(request):
-    menu_items = MenuItem.objects.all()
-    return render(request, 'foodie_buddies/menu_item_list.html', {'menu_items': menu_items})
-
-
-def menu_item_detail(request, pk):
-    menu_item = MenuItem.objects.get(id=pk)
-    return render(request, 'foodie_buddies/menu_item_detail.html', {'menu_item': menu_item})
-
-def comment_detail(request, pk):
-    comment = Comment.objects.get(id=pk)
-    return render(request, 'foodie_buddies/comment_detail.html', {'comment': comment})
-
 def restaurant_create(request):
     if request.method == 'POST':
         form = RestaurantForm(request.POST)
@@ -36,6 +23,34 @@ def restaurant_create(request):
     else:
         form = RestaurantForm()
     return render(request, 'foodie_buddies/restaurant_form.html', {'form': form})
+
+
+def restaurant_update(request, pk):
+    restaurant = Restaurant.objects.get(id=pk)
+    if request.method == 'POST':
+        form = RestaurantForm(request.body, instance=restaurant)
+        if form.is_valid:
+            restaurant = form.save()
+            return redirect('restaurant_detail', id=restaurant.id)
+    else:
+        form = RestaurantForm(instance=restaurant)
+        return render(request, 'foodie_buddies/restaurant_form.html', {'form': form})
+
+
+def restaurant_delete(request, pk):
+    if request.method == 'POST':
+        Restaurant.objects.get(id = pk).delete()
+    return redirect('restaurant_list')
+
+
+def menu_item_list(request):
+    menu_items = MenuItem.objects.all()
+    return render(request, 'foodie_buddies/menu_item_list.html', {'menu_items': menu_items})
+
+
+def menu_item_detail(request, pk):
+    menu_item = MenuItem.objects.get(id=pk)
+    return render(request, 'foodie_buddies/menu_item_detail.html', {'menu_item': menu_item})
 
 
 def menu_item_create(request):
@@ -49,6 +64,28 @@ def menu_item_create(request):
     return render(request, 'foodie_buddies/menu_item_form.html', {'form': form})
 
 
+def menu_item_update(request, pk):
+    menu_item = MenuItem.objects.get(id=pk)
+    if request.method == 'POST':
+        form = MenuItemForm(request.body, instance=menu_item)
+        if form.is_valid:
+            menu_item = form.save()
+            return redirect('menu_item_detail', id=menu_item.id)
+    else:
+        form = MenuItemForm(instance=menu_item)
+        return render(request, 'foodie_buddies/menu_item_form.html', {'form': form})
+
+def menu_item_delete(request, pk):
+    if request.method == 'POST':
+        MenuItem.objects.get(id = pk).delete()
+    return redirect('menu_item_list')
+
+
+def comment_detail(request, pk):
+    comment = Comment.objects.get(id=pk)
+    return render(request, 'foodie_buddies/comment_detail.html', {'comment': comment})
+
+
 def comment_create(request):
     if request.method == 'POST':
         form = CommentForm(request.POST)
@@ -58,3 +95,22 @@ def comment_create(request):
     else:
         form = CommentForm()
     return render(request, 'foodie_buddies/comment_form.html', {'form': form})
+
+
+def comment_update(request, pk):
+    comment = Comment.objects.get(id=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.body, instance=comment)
+        if form.is_valid:
+            comment = form.save()
+            return redirect('comment_detail', id=comment.id)
+    else:
+        form = CommentForm(instance=comment)
+        return render(request, 'foodie_buddies/comment_form.html', {'form': form})
+
+
+def comment_delete(request, pk):
+    if request.method == 'POST':
+        Comment.objects.get(id = pk).delete()
+    return redirect('comment_list')
+	 
